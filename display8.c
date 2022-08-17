7749525cfdcc2c7d859ce8ffa3f25ec7908e9f88
Shubham Kedar <84714233+shubhamkedar@users.noreply.github.com>
2022-08-17 18:35:53 +0530

    Add files via upload

---
+++ display8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void display();
+int main()
+{
+display();
+return 0;
+}
+void display()
+{
+int i=0;
+for(i=1;i<=8;i++)
+{
+printf("#\n");
+}
+}
---
+++ displayfor1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void Display()
+{
+int i;
+for(i=1;i<=5;i++)
+{
+printf("%d\n",i);
+}
+}
+int main()
+{
+Display();  //function cll
+return 0;
+}
