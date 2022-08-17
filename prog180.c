0d7820631f77bc0a1f22d25a9a12ff07f3b3408e
Shubham Kedar <84714233+shubhamkedar@users.noreply.github.com>
2022-08-17 18:40:29 +0530

    Add files via upload

---
+++ prog180.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+	{
+		int fd=0,iCnt=0,iRet=0,i=0;
+		char fname[20];
+		char Buffer[10];
+		printf("Enter name of file:");
+		scanf("%s",fname);
+		fd=open(fname,O_RDWR);
+		if(fd==-1)
+		{
+			printf("Unable to Open file\n");
+			return -1;
+		}
+		
+		while((iRet=read(fd,Buffer,10))!=0)
+		{
+			for(i=0;i<iRet;i++)
+			{
+				if(Buffer[i]>='0'&&Buffer[i]<='9')
+				{
+					iCnt++;
+				}
+			}
+		}
+		printf("Total no of Small letters  are :%d\n",iCnt);
+		close(fd);
+		
+		
+		return 0;
+	}
+	
